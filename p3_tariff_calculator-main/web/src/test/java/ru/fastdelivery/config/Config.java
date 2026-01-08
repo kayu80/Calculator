ff6a47f9303d293af25bc8bbec8b7a65190e178c
kayu80 <Kayu80@mail.ru>
2026-01-08 22:16:37 +0300

    Delete p3_tariff_calculator-main/web/src/test/java/ru/fastdelivery/config/Config.java

--- p3_tariff_calculator-main/web/src/test/java/ru/fastdelivery/config/Config.java
+++
@@ -1,9 +0,0 @@
-package ru.fastdelivery.config;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan(basePackages = { "ru.fastdelivery" })
-@EnableAutoConfiguration
-public class Config {
-}
