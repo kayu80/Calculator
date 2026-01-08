fee3f3a47ab87365487af786b7bcb7da3776c90f
kayu80 <Kayu80@mail.ru>
2026-01-08 22:19:48 +0300

    Update Beans.java

--- p3_tariff_calculator-main/app/src/main/java/ru/fastdelivery/config/Beans.java
+++ p3_tariff_calculator-main/app/src/main/java/ru/fastdelivery/config/Beans.java
@@ -5,21 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import ru.fastdelivery.domain.common.currency.CurrencyFactory;
 import ru.fastdelivery.domain.common.currency.CurrencyPropertiesProvider;
 import ru.fastdelivery.usecase.TariffCalculateUseCase;
+import ru.fastdelivery.usecase.VolumePriceProvider;
 import ru.fastdelivery.usecase.WeightPriceProvider;
 
-/**
- * Определение реализаций бинов для всех модулей приложения
- */
 @Configuration
 public class Beans {
 
     @Bean
-    public CurrencyFactory currencyFactory(CurrencyPropertiesProvider currencyProperties) {
-        return new CurrencyFactory(currencyProperties);
+    public CurrencyFactory currencyFactory(CurrencyPropertiesProvider provider) {
+        return new CurrencyFactory(provider);
     }
 
     @Bean
-    public TariffCalculateUseCase tariffCalculateUseCase(WeightPriceProvider weightPriceProvider) {
-        return new TariffCalculateUseCase(weightPriceProvider);
+    public TariffCalculateUseCase tariffCalculateUseCase(WeightPriceProvider weightPriceProvider, VolumePriceProvider volumePriceProvider) {
+        return new TariffCalculateUseCase(weightPriceProvider, volumePriceProvider);
     }
 }
